b9833f3fddfdc30cce33bf5309a6b9382b68eb20
Paul Schmiedmayer <PSchmiedmayer@users.noreply.github.com>
2023-02-14 01:17:23 -0800

    Update Tests

--- TemplateApplicationUITests/HealthKitUploadTests.swift
+++ TemplateApplicationUITests/HealthKitUploadTests.swift
@@ -29,13 +29,14 @@ class HealthKitUploadTests: XCTestCase {
         try app.conductOnboardingIfNeeded()
         
         try navigateToMockUpload()
-        try assertObservationCellPresent(false)
         
-        app.terminate()
+        try assertObservationCellPresent(false)
         
         try exitAppAndOpenHealth(.steps)
         
         app.activate()
+        
+        sleep(5)
         
         try navigateToMockUpload()
         try assertObservationCellPresent(true, pressIfPresent: true)
