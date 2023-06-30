8b26114204a0b58cf0c153d8ddb875c9a171a514
Paul Schmiedmayer <PSchmiedmayer@users.noreply.github.com>
2023-06-30 11:19:47 -0700

    Enable Code QL and use Swift 5.7

--- TemplateApplication/Schedule/TemplateApplicationScheduler.swift
+++ TemplateApplication/Schedule/TemplateApplicationScheduler.swift
@@ -19,47 +19,32 @@ typealias TemplateApplicationScheduler = Scheduler<FHIR, TemplateApplicationTask
 extension TemplateApplicationScheduler {
     /// Creates a default instance of the ``TemplateApplicationScheduler`` by scheduling the tasks listed below.
     convenience init() {
+        let dateComponents: DateComponents
+        if FeatureFlags.testSchedule {
+            // Adds a task at the current time for UI testing if the `--testSchedule` feature flag is set
+            dateComponents = DateComponents(
+                hour: Calendar.current.component(.hour, from: .now),
+                minute: Calendar.current.component(.minute, from: .now)
+            )
+        } else {
+            // For the normal app usage, we schedule the task for every day at 8:00 AM
+            dateComponents = DateComponents(hour: 8, minute: 0)
+        }
+        
         var tasks: [SpeziScheduler.Task<TemplateApplicationTaskContext>] = [
             Task(
                 title: String(localized: "TASK_SOCIAL_SUPPORT_QUESTIONNAIRE_TITLE"),
                 description: String(localized: "TASK_SOCIAL_SUPPORT_QUESTIONNAIRE_DESCRIPTION"),
                 schedule: Schedule(
                     start: Calendar.current.startOfDay(for: Date()),
-                    repetition: .matching(.init(hour: 8, minute: 0)), // Every Day at 8:00 AM
+                    repetition: .matching(dateComponents),
                     end: .numberOfEvents(365)
                 ),
                 notifications: true,
                 context: TemplateApplicationTaskContext.questionnaire(Bundle.main.questionnaire(withName: "SocialSupportQuestionnaire"))
             )
         ]
-
-        /// Adds a task at the current time for UI testing if the `--testSchedule` feature flag is set
-        if FeatureFlags.testSchedule || FeatureFlags.testScheduleNotification {
-            let currentDate = Date.now
-            let currentHour = Calendar.current.component(.hour, from: currentDate)
-            let currentMinute: Int
-            if FeatureFlags.testScheduleNotification {
-                // We expect the UI test to take at least 20 seconds to mavigate out of the app and to the home screen.
-                // We then trigger the task in the minute after that, the UI test needs to wait at least one minute.
-                currentMinute = Calendar.current.component(.minute, from: currentDate.addingTimeInterval(20)) + 1
-            } else {
-                currentMinute = Calendar.current.component(.minute, from: currentDate)
-            }
-            
-            let testTask = Task(
-                title: String(localized: "TASK_TEST_TITLE"),
-                description: String(localized: "TASK_TEST_DESCRIPTION"),
-                schedule: Schedule(
-                    start: Calendar.current.startOfDay(for: currentDate),
-                    repetition: .matching(.init(hour: currentHour, minute: currentMinute)), // repeat at current time
-                    end: .numberOfEvents(1)
-                ),
-                notifications: FeatureFlags.testScheduleNotification,
-                context: TemplateApplicationTaskContext.test(String(localized: "TASK_TEST_CONTENT"))
-            )
-            tasks.append(testTask)
-        }
-
+        
         self.init(tasks: tasks)
     }
 }
