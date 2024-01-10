b7f8fa85a222ba3463deb100c7c61bca21ed484f
Paul Schmiedmayer <PSchmiedmayer@users.noreply.github.com>
2024-01-09 23:22:46 -0800

    Add Setup Script

---
+++ Scripts/create.sh
@@ -0,0 +1,15 @@
+#!/bin/s
+#
+# This source file is part of the Stanford Spezi Template Application project
+#
+# SPDX-FileCopyrightText: 2022 Stanford University and the project authors (see CONTRIBUTORS.md)
+#
+# SPDX-License-Identifier: MIT
+#
+
+# Script to create a new iOS application based on the Spezi Template Application
+#
+# Arguments:
+# --test : ...
+
+# 1. ...
