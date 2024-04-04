8263dce9dcb9fb6a7426eccdfd5b1fe52e6242e4
NikolaiMadlener <nikolai@madlener.io>
2024-04-04 08:44:02 +0200

    update SpeziLicense dependency to version 0.1.0

--- TemplateApplication/Account/AccountSheet.swift
+++ TemplateApplication/Account/AccountSheet.swift
@@ -27,7 +27,7 @@ struct AccountSheet: View {
                 if account.signedIn && !isInSetup {
                     AccountOverview(isEditing: $overviewIsEditing) {
                         NavigationLink {
-                            ContributionsList(projectLicense: "MIT")
+                            ContributionsList(projectLicense: .mit)
                         } label: {
                             Text("LICENSE_INFO_TITLE")
                         }
