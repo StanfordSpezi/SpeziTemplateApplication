68844497fb21f181fb01900833645d2467242535
Paul Schmiedmayer <PSchmiedmayer@users.noreply.github.com>
2024-06-07 15:51:00 -0700

    Remove duplicated code

--- TemplateApplicationTests/TemplateApplicationTests.swift
+++ TemplateApplicationTests/TemplateApplicationTests.swift
@@ -6,33 +6,12 @@
 // SPDX-License-Identifier: MIT
 //
 
-import SwiftUI
 @testable import TemplateApplication
 import XCTest
 
 
 class TemplateApplicationTests: XCTestCase {
-    func testNegateBinding() throws {
-        let trueBinding = Binding(
-            get: { true },
-            set: { newValue in
-                XCTAssertTrue(newValue)
-            }
-        )
-        let falseBinding = Binding(
-            get: { false },
-            set: { newValue in
-                XCTAssertFalse(newValue)
-            }
-        )
-        
-        let negatedTrueBinding = !trueBinding
-        let negatedFalseBinding = !falseBinding
-        
-        XCTAssertFalse(negatedTrueBinding.wrappedValue)
-        XCTAssertTrue(negatedFalseBinding.wrappedValue)
-        
-        negatedTrueBinding.wrappedValue = false
-        negatedFalseBinding.wrappedValue = true
+    func testContactsCount() throws {
+        XCTAssertEqual(Contacts(presentingAccount: .constant(true)).contacts.count, 1)
     }
 }
