7e70b1806aeed06b76c0dfc174439777078f0565
Vishnu Ravi <vishnur@stanford.edu>
2024-11-10 13:37:25 -0500

    Update log manager

--- TemplateApplication/Logging/LogManager.swift
+++ TemplateApplication/Logging/LogManager.swift
@@ -12,36 +12,40 @@ import Spezi
 import SwiftUI
 
 class LogManager {
+    private let store: OSLogStore?
+
+    init() {
+        self.store = try? OSLogStore(scope: .currentProcessIdentifier)
+    }
+
     func query(
         startDate: Date,
         endDate: Date? = nil,
         logLevel: OSLogEntryLog.Level? = nil
     ) throws -> [OSLogEntryLog] {
-        let store = try OSLogStore(scope: .currentProcessIdentifier)
-        let position = store.position(date: startDate)
-        
-        guard let bundleIdentifier = Bundle.main.bundleIdentifier else {
+        guard let store,
+              let bundleIdentifier = Bundle.main.bundleIdentifier  else {
             return []
         }
-        
+
+        let position = store.position(date: startDate)
         let predicate = NSPredicate(format: "subsystem == %@", bundleIdentifier)
-        
         let logs = try store.getEntries(at: position, matching: predicate)
             .reversed()
             .compactMap { $0 as? OSLogEntryLog }
-        
+
         return logs
             .filter { logEntry in
                 /// Filter by log type if specified
                 if let logLevel, logEntry.level != logLevel {
                     return false
                 }
-                
+
                 /// Filter by end date if specified
                 if let endDate, logEntry.date > endDate {
                     return false
                 }
-                
+
                 return true
             }
     }
