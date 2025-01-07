7fc13fd9e312f2353eaa2791e8dae7f255b86ef6
Vishnu Ravi <vishnur@stanford.edu>
2025-01-07 16:23:00 -0500

    Fix heredoc

--- Scripts/setup.sh
+++ Scripts/setup.sh
@@ -11,40 +11,42 @@
 #
 # It is required that Xcode is installed on the macOS instance.
 
-# 1. Install homebrew
-export NONINTERACTIVE=1
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ~/.zprofile
-eval "$(/opt/homebrew/bin/brew shellenv)"
+# # 1. Install homebrew
+# export NONINTERACTIVE=1
+# /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+# echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ~/.zprofile
+# eval "$(/opt/homebrew/bin/brew shellenv)"
 
 
-# 2. Install tools
-brew install java
-sudo ln -sfn /opt/homebrew/opt/openjdk/libexec/openjdk.jdk /Library/Java/JavaVirtualMachines/openjdk.jdk
-echo 'export PATH="/opt/homebrew/opt/openjdk/bin:$PATH"' >> ~/.zshrc
+# # 2. Install tools
+# brew install java
+# sudo ln -sfn /opt/homebrew/opt/openjdk/libexec/openjdk.jdk /Library/Java/JavaVirtualMachines/openjdk.jdk
+# echo 'export PATH="/opt/homebrew/opt/openjdk/bin:$PATH"' >> ~/.zshrc
 
-brew install node
-brew install firebase-cli
-brew install fastlane
-# Set the local correctly to work with fastlane
-echo 'export LC_ALL=en_US.UTF-8' >> ~/.zshrc
-echo 'export LANG=en_US.UTF-8' >> ~/.zshrc
+# brew install node
+# brew install firebase-cli
+# brew install fastlane
+# # Set the local correctly to work with fastlane
+# echo 'export LC_ALL=en_US.UTF-8' >> ~/.zshrc
+# echo 'export LANG=en_US.UTF-8' >> ~/.zshrc
 
-brew install git-lfs
-git lfs install
-git lfs install --system
+# brew install git-lfs
+# git lfs install
+# git lfs install --system
 
-# Ensure that everything on the system is up-to-date
-brew upgrade
+# # Ensure that everything on the system is up-to-date
+# brew upgrade
 
 
 # 3. Test and start the firebase emulator
 
 # Check if firebase.json exists and create if it doesn't
+CREATED_FIREBASE_JSON=false
+
 if [ ! -f "firebase.json" ]; then
   echo "Creating firebase.json file..."
   CREATED_FIREBASE_JSON=true
-  cat > firebase.json << 'EOL'
+  cat << 'EOL' > firebase.json
 {
   "firestore": {
     "rules": "firestore.rules"
@@ -70,6 +72,7 @@ if [ ! -f "firebase.json" ]; then
   }
 }
 EOL
+fi
 
 firebase emulators:exec --project test "echo 'Firebase emulator installed and started successfully!'"
 
